a1fa796244a44d67e6361ba727208e4890089ce4
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2023-06-23 18:22:21 +0400

    some changes

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)
+        onChange && onChange(e)
 
         // задачка на написание онченджа
         // ЕСЛИ(onChangeChecked ВООБЩЕ СУЩЕСТВУЕТ){
