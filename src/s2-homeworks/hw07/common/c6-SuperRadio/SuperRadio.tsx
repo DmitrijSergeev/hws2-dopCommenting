0f93f9e495bf49858e1ef66effe64d210a539778
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2023-07-15 19:47:06 +0400

    7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,7 +51,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      name={o.name}
+                      name={o.value}
                       checked={o.id === Number(value)}
                       value={o.id}
                       // name, checked, value делают студенты
