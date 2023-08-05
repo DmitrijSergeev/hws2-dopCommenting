7332b6b91f76a9bf5ff951866203809f66a56212
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2023-08-05 15:38:46 +0400

    9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -36,8 +36,8 @@ function Clock() {
     const stringDate =new Intl.DateTimeFormat(['ru'],{day: 'numeric', month: 'numeric', year: 'numeric'}).format(date.getDate())   || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.getDay() || <br/> // пишут студенты
-    const stringMonth = date.getMonth() || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat().format(date.getDay() )  || <br/> // пишут студенты
+    const stringMonth = new Intl.DateTimeFormat().format(date.getMonth())  || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
